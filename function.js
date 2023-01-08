cdf5d5f7289d13e5c90cb2185db9dac58fa73a32
Md. Nazmul Hasan Moon <nazmulmoon123@gmail.com>
2023-01-08 19:20:50 +0600

    Some basic examples of JS user defined functons

---
+++ function.js
@@ -0,0 +1,24 @@
+let hello = fun();
+console.log(hello);
+function fun(){
+    return "hello world!";
+}
+hello = ()=>{
+    return "hello Bangladesh";
+}
+console.log(hello());
+
+hello = ()=>"hello Nazmul";
+console.log(hello());
+let y =  document.getElementById("username").value;
+hello = (val)=>"hello "+val;
+console.log(hello(y));
+hello = function(){
+    return "Hello Folks";
+}
+console.log(hello());
+function myFunction(){
+    let x = document.getElementById("username").value;
+    console.log(x);
+    document.getElementById("demo").innerHTML = x;
+}
