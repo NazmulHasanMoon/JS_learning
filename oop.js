933209368900707ec573c21520a4903488563b79
Md. Nazmul Hasan Moon <nazmulmoon123@gmail.com>
2023-02-04 12:34:00 +0600

    Example of getter and setter in JS

--- oop.js
+++ oop.js
@@ -4,10 +4,19 @@ class Human{
         this.age = age;
     }
     getInfo() {
-        return "Name of this person is "+ this.name +" and age is "+this.age;
+        return "Name of this person is "+ this.name;
+    }
+    get pAge(){
+        return this.age;
+    }
+    set pAge(age){
+        this.age = age;
     }
 }
 const person1 = new Human("Nazmul",27);
 const person2 = new Human("Sohag",27);
 console.log(person1.getInfo());
 console.log(person2.getInfo());
+console.log(person1.pAge);
+person2.pAge = 28;
+console.log(person2.pAge);
