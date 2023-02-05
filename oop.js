9e35c626f57ee9c0e889e92ed0062feb82d62883
Md. Nazmul Hasan Moon <nazmulmoon123@gmail.com>
2023-02-05 12:55:37 +0600

    Inheritance in JS

--- oop.js
+++ oop.js
@@ -6,6 +6,9 @@ class Human{
     getInfo() {
         return "Name of this person is "+ this.name;
     }
+    get pName(){
+        return this.name;
+    }
     get pAge(){
         return this.age;
     }
@@ -20,3 +23,18 @@ console.log(person2.getInfo());
 console.log(person1.pAge);
 person2.pAge = 28;
 console.log(person2.pAge);
+
+//inheritance
+class Student extends Human{
+    constructor(name,age,dept){
+        super(name,age);
+        this.dept=dept;
+    }
+    getStudentInfo(){
+        return "The name of the Student is "+ this.name+". He/She is "+this.age+" and student of "+this.dept; 
+    }
+}
+let std1 = new Student("Moon",26,"Post Graduation");
+console.log(std1.getStudentInfo());
+console.log(std1.pName);
+console.log(std1.getInfo());
